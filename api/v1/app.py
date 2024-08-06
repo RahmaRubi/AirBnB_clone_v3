c6b29379949a1f1cbc158d191694a11a755ec7de
youssef-elmelegy <youssefelmelegy999@gmail.com>
2024-08-05 17:52:47 -0700

    task amenities

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,10 +4,12 @@ from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
